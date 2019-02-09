957081756e26b89144f7aa7242008b37a51bbae1
Axel Gschaider <axel.gschaider@posteo.de>
2019-02-10 00:54:19 +0100

    moved in standard #!++ files

---
+++ crunchBangPlusPlus/rolout.sh
@@ -0,0 +1,9 @@
+ln -f ./conkyrc ~/.conkyrc
+ln -f compton.conf ~/.config/compton.conf
+mkdir -p ~/.config/dmenu/
+ln -f dmenu-bind.sh ~/.config/dmenu/dmenu-bind.sh
+mkdir -p ~/.config/openbox/
+ln -f openbox/autostart ~/.config/openbox/autostart
+ln -f openbox/menu.xml ~/.config/openbox/menu.xml
+ln -f openbox/rc.xml ~/.config/openbox/rc.xml
+mkdir -p ~/.config/tint2/
